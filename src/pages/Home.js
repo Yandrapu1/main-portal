1459a51d45ed351bd011261c59cb60ff797f9fe0
pradeeppoppin <yandrapupradeep@gmail.com>
2025-04-02 10:46:07 +0530

    Updated project files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => (
       <div className="card">
         
       <img 
-          src="https://i.postimg.cc/pd3SdHtJ/core-img.jpg" 
+          src="https://i.postimg.cc/B6bVByWD/1701088302650.jpg" 
           alt="Software Development Logo" 
           className="card-logo-software" 
         />
@@ -19,13 +19,13 @@ const Home = () => (
         
         <p>Guides for mastering engineering fundamentals.</p>
         
-         <a href="https://coreengineering.ccbp.tech/" class="btn">ViewAll</a>
+         <a href="https://coreengineering.ccbp.tech/" class="btn">View All</a>
 
    </div>
  
       <div className="card">
         <img 
-          src="https://i.postimg.cc/zDxYppFb/downloadpp.png" 
+          src="https://i.postimg.cc/RFnVbgK6/images.jpg " 
           alt="Software Development Logo" 
           className="card-logo-software" 
         />
@@ -45,7 +45,7 @@ const Home = () => (
         />
         <h2>GATE Preparation</h2>
         <p>Resources for cracking the GATE exam.</p>
-         <a href="https://gateexam.ccbp.tech/" class="btn">ViewAll</a>
+         <a href="https://gateexam.ccbp.tech/" class="btn">View All</a>
 
 
       </div>
@@ -58,21 +58,21 @@ const Home = () => (
         <h2>Specialized Fields</h2>
         <p>Focused guides for niche areas of study.</p>
      
-         <a href="https://specialized.ccbp.tech" class="btn">ViewAll</a>
+         <a href="https://specialized.ccbp.tech" class="btn">View All</a>
 
       </div>
 
       {/* Resources Section */}
       <div className="card">
       <img 
-          src="https://i.postimg.cc/nVBjsWtt/wallpaper.webp" 
+          src="https://i.postimg.cc/FFwvsZmH/images.jpg" 
           alt="Software Development Logo" 
           className="card-logo-software" 
         />
         <h2>Placement <br></br> Prep</h2>
         <p>Interview questions and coding challenges.</p>
       
-       <a href="https://placemeinner.ccbp.tech/ " class="btn">ViewAll</a>
+       <a href="https://placementcol.ccbp.tech " class="btn">ViewAll</a>
 
       </div>
       <div className="card">
@@ -83,7 +83,7 @@ const Home = () => (
         />
         <h2>Academic Materials</h2>
         <p>Lecture notes and e-books for your studies.</p>
-         <a href="https://academic.ccbp.tech" class="btn">ViewAll</a>
+         <a href="https://academic.ccbp.tech" class="btn">View All</a>
 
       </div>
       <div className="card">
@@ -94,7 +94,7 @@ const Home = () => (
         />
         <h2>Free  <br></br>Courses</h2>
         <p>Skill-building courses with certifications.</p>
-       <a href="https://freecou.ccbp.tech" class="btn">ViewAll</a>
+       <a href="https://freecou.ccbp.tech" class="btn">View All</a>
 
       </div>
       <div className="card">
@@ -106,49 +106,49 @@ const Home = () => (
         <h2>Video  <br></br>Tutorials</h2>
         <p>Comprehensive video guides on key topics.</p>
       
-         <a href="https://videotutorials.ccbp.tech/" class="btn">ViewAll</a>
+         <a href="https://videotutorials.ccbp.tech/" class="btn">View All</a>
 
       </div>
 
       {/* Mentorship & Events Section */}
       <div className="card">
       <img 
-          src="https://i.postimg.cc/t4ZzB4yG/senior-logo.png" 
+          src="https://i.postimg.cc/RZ2JXJn0/Mentorship.jpg" 
           alt="Software Development Logo" 
           className="card-logo-software" 
         />
-        <h2>Connect with Seniors</h2>
+        <h2> Mentorship</h2>
         <p>Guidance from experienced peers.</p>
-         <a href="https://softwaredevelop.ccbp.tech/" class="btn">ViewAll</a>
+         <a href="https:connectseniors.ccbp.tech" class="btn">View All</a>
 
       </div>
       <div className="card">
       <img 
-          src="https://i.postimg.cc/5NM1467n/Expert-Talks-logo.webp" 
+          src="https://i.postimg.cc/T3f4hhPp/download.jpg" 
           alt="Software Development Logo" 
           className="card-logo-software" 
         />
         <h2>Expert Talks</h2>
         <p>Sessions from industry professionals.</p>
       
-         <a href="https://softwaredevelop.ccbp.tech/" class="btn">ViewAll</a>
+         <a href="https://softwaredevelop.ccbp.tech/" class="btn">View All</a>
 
       </div>
       <div className="card">
       <img 
-          src="https://i.postimg.cc/6Q1ttcgv/Workshops-logo.jpg" 
+          src="https://i.postimg.cc/D03K5vxb/images.jpg" 
           alt="Software Development Logo" 
           className="card-logo-software" 
         />
         <h2>Workshops</h2>
         <p>Skill-enhancing academic and technical workshops.</p>
       
-        <a href="https://softwaredevelop.ccbp.tech/" class="btn">ViewAll</a>
+        <a href="https://softwaredevelop.ccbp.tech/" class="btn">View All</a>
 
       </div>
       <div className="card">
       <img 
-          src="https://i.postimg.cc/Njdwkbgz/Hackathons-logo.png" 
+          src="https://i.postimg.cc/jjCKSkWC/download.png" 
           alt="Software Development Logo" 
           className="card-logo-software" 
         />
@@ -157,7 +157,7 @@ const Home = () => (
       
         <p>Compete and showcase your innovative ideas.</p>
       
-         <a href="https://softwaredevelop.ccbp.tech/" class="btn">ViewAll</a>
+         <a href="https://softwaredevelop.ccbp.tech/" class="btn">View All</a>
 
       </div>
     </div>
